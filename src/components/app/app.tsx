0bded6e827c5508518fb5dd2551abc068baadf1f
Алексей Пыханов <apykhanov@gmail.com>
2024-03-22 22:48:15 +0300

    Маршрут по тайным тропам

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
+import {HelmetProvider} from 'react-helmet-async';
 
 type AppProps = {
   cardsCount: number;
@@ -6,7 +14,40 @@ type AppProps = {
 
 function App({cardsCount}: AppProps): JSX.Element {
   return (
-    <MainScreen cardsCount={cardsCount} />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Root}
+            element={<MainScreen cardsCount={cardsCount} />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.NoAuth}
+              >
+                <LoginScreen />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<LoginScreen />}
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<OfferScreen />}
+          />
+          <Route
+            path="*"
+            element={<NotFoundScreen />}
+          />
+          <Route path={`${AppRoute.Offer}/:OfferId`} element={<OfferScreen />} />
+          <Route path={'*'} element ={<NotFoundScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,16 @@
 export const Setting = {
   CardsCount: 6
 };
+
+export enum AppRoute {
+  Login = '/login',
+  Offer = '/Offer',
+  Favorites = '/Favorites',
+  Root = '/',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
